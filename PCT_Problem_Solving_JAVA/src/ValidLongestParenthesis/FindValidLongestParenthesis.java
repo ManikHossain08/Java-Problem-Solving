47c794b28c7c1f1140462c495289a7141299fa10
ManikHossain08 <manik083017@gmail.com>
2020-07-14 13:28:44 -0400

    solve the problem for finding thee largest length of valid  parenthesis in java

---
+++ PCT_Problem_Solving_JAVA/src/ValidLongestParenthesis/FindValidLongestParenthesis.java
@@ -0,0 +1,41 @@
+package ValidLongestParenthesis;
+
+import java.util.Scanner;
+
+public class FindValidLongestParenthesis {
+	public static void main(String args[]) {
+
+		Scanner sc_in = new Scanner(System.in);
+		String inputStr = sc_in.nextLine();
+		
+		System.out.print(longestValidParentheses(inputStr));
+		sc_in.close();
+	}
+
+	// Important:
+	// https://www.geeksforgeeks.org/median-two-sorted-arrays-different-sizes-ologminn-m/
+	public static int longestValidParentheses(String s) {
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < s.length(); i++) {
+			count = 0;
+			for (int j = i; j < s.length(); j++) {
+				if (s.charAt(j) == '(') {
+					count++;
+				} else {
+					count--;
+				}
+				if (count < 0) {
+					break;
+
+				}
+				if (count == 0) {
+					if (j - i + 1 > max) {
+						max = j - i + 1;
+					}
+				}
+			}
+		}
+		return max;
+	}
+}
