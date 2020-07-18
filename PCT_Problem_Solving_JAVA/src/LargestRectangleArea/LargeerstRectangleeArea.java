a4fe1706165f19ff4d6e8ab3c62629084ecde505
ManikHossain08 <manik083017@gmail.com>
2020-07-18 13:47:56 -0400

    although solved the problem but did not understand properly

---
+++ PCT_Problem_Solving_JAVA/src/LargestRectangleArea/LargeerstRectangleeArea.java
@@ -0,0 +1,59 @@
+package LargestRectangleArea;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class LargeerstRectangleeArea {
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		int inputNo = sc.nextInt();
+		int[] heightOfRectangle = new int[inputNo];
+		for (int i = 0; i < inputNo; i++) {
+			heightOfRectangle[i] = sc.nextInt();
+		}
+
+		System.out.print(findLargestRectangularArea(heightOfRectangle));
+		sc.close();
+
+	}
+
+	private static int findLargestRectangularArea(int[] heights) {
+	
+		if (heights.length == 0)
+			return 0;
+
+		int[] firstSmallerOntheLeft = new int[heights.length];
+		int[] firstSmallerOntheRight = new int[heights.length];
+
+		Arrays.fill(firstSmallerOntheLeft, -1);
+		Arrays.fill(firstSmallerOntheRight, heights.length);
+
+		Stack<Integer> stack = new Stack<Integer>();
+		
+
+		for (int i = 0; i < heights.length; i++) {
+			while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+				firstSmallerOntheRight[stack.pop()] = i;
+			}
+			if (!stack.isEmpty())
+				firstSmallerOntheLeft[i] = stack.peek();
+			
+			stack.push(i);
+		}
+
+		int res = 0;
+
+		for (int i = 0; i < heights.length; i++) {
+
+			int l = firstSmallerOntheLeft[i];
+			int r = firstSmallerOntheRight[i];
+			res = Math.max(res, (r - l - 1) * heights[i]);
+			
+		}
+
+		return res;
+	}
+
+}
