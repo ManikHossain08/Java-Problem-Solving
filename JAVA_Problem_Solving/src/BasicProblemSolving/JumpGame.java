8e942d75a0dc575d7bb88c7900dd53d407365d9d
ManikHossain08 <manik083017@gmail.com>
2020-07-30 18:31:36 -0400

    Solved Jump game in java actually learned from this code

---
+++ JAVA_Problem_Solving/src/BasicProblemSolving/JumpGame.java
@@ -0,0 +1,33 @@
+package BasicProblemSolving;
+
+import java.util.Scanner;
+
+public class JumpGame {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int elemNum = sc.nextInt();
+		int[] arrInp = new int[elemNum];
+		for (int i = 0; i < elemNum; i++) {
+			arrInp[i] = sc.nextInt();
+		}
+		int position = 0;
+
+		System.out.print(canJumpFromPosition(position, arrInp));
+		sc.close();
+	}
+
+	public static boolean canJumpFromPosition(int position, int[] nums) {
+		if (position == nums.length - 1) {
+			return true;
+		}
+
+		int furthestJump = Math.min(position + nums[position], nums.length - 1);
+		for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+			if (canJumpFromPosition(nextPosition, nums)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
