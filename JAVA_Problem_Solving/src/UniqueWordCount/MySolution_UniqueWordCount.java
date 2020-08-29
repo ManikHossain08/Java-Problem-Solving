377d9e9411d23c32f9c73203e443eff8ecf001aa
ManikHossain08 <manik083017@gmail.com>
2020-08-29 14:06:10 -0400

    solved uniqe word count in java from PCT questions_basic problem in java

---
+++ JAVA_Problem_Solving/src/UniqueWordCount/MySolution_UniqueWordCount.java
@@ -0,0 +1,25 @@
+package UniqueWordCount;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class MySolution_UniqueWordCount {
+public static void main(String args[]) {
+		
+		Scanner sc_in = new Scanner(System.in);
+		String[] inputStr = sc_in.nextLine().split(" ");
+		
+		Set<String> uniqueList = new HashSet<String>();
+		System.out.print(finduniqueWord(inputStr, uniqueList));
+		
+		sc_in.close();
+	}
+
+	private static int finduniqueWord(String[] inputStr, Set<String> uniqueList) {
+		for (int i = 0; i < inputStr.length; i++) {
+			uniqueList.add(inputStr[i].toLowerCase());
+		}
+		return uniqueList.size();
+	}
+}
